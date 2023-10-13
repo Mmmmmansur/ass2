4b7e0276d3b554e32aaf73e6e6eca4aae6288e14
Mmmmmansur <147161346+Mmmmmansur@users.noreply.github.com>
2023-10-13 18:15:38 +0600

    Add files via upload

---
+++ Mansur/src/AbstractFactoryMethod/ComputerAbstractFactory.java
@@ -0,0 +1,5 @@
+package AbstractFactoryMethod;
+
+public interface ComputerAbstractFactory { // интерфейс для создания компutera
+    public Computer createComputer(); // возвращает обьект класса компьютер
+}
